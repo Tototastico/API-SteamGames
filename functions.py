872df99654ad361d15c3e8704987947fd6c0928d
Tobias Sirne <sirnetobias1@gmail.com>
2023-09-03 14:22:23 -0300

    commit

--- functions.py
+++ functions.py
@@ -1,14 +1,11 @@
 import pandas as pd
 import pickle
 
-with open('clean_items_genreFunction.pkl', 'rb') as f:
-    df_items = pickle.load(f)
-with open('clean_games_genreFunction.pkl', 'rb') as f:
-    df_games = pickle.load(f)
-
 def genre(genero: str): # Esta funcion es a que mas se demora
     #def genre( género : str ): Devuelve el puesto en el que se encuentra un género
     #sobre el ranking de los mismos analizado bajo la columna PlayTimeForever.
+    df_games = pd.read_pickle('clean_games_genreFunction.pkl')
+    df_items = pd.read_pickle('clean_items_genreFunction.pkl')
     generos_unicos = df_games['genres'].unique() # Extraemos nuestros generos unicos
     sumas_por_genero = {} # Instanciamos un diccionario
     for gen in generos_unicos: # Recorremos nuestros generos
