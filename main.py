f87b97199ee19657fcc10359d368aee066463cc5
Tobias Sirne <sirnetobias1@gmail.com>
2023-09-03 15:27:18 -0300

    commit

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ df_reviews = pd.read_parquet('clean_reviews.parquet.gzip')
 df_games = pd.read_parquet('clean_games.parquet.gzip')
 df_items = pd.read_parquet('clean_items_functions.parquet.gzip')
 
-@app.get('/userdata/{User_id}')
+@app.get('/userdata/{User_id}', response_class=HTMLResponse)
 def userdata(User_id: int):
     User_id = str(User_id)
     user_games = df_items[df_items['user_id'] == User_id]['item_id']
@@ -31,9 +31,9 @@ def userdata(User_id: int):
     user_recomendations = df_reviews[df_reviews['user_id'] == User_id]['recommend'].tolist()
     user_recomendations = sum(user_recomendations)
     percentage = user_recomendations/total_games
-    return [total_amount, f'{round(percentage*100,2)}%']
+    return f'<p>{total_amount} : {round(percentage*100,2)}%</p>'
 
-@app.get('/countreviews/{fecha_inicio},{fecha_fin}') # Aca separamos nuestros parametros en la ruta con una ','
+@app.get('/countreviews/{fecha_inicio},{fecha_fin}', response_class=HTMLResponse) # Aca separamos nuestros parametros en la ruta con una ','
 def countreviews(fecha_inicio:str, fecha_fin:str):
     #def countreviews( YYYY-MM-DD y YYYY-MM-DD : str ): Cantidad de usuarios que realizaron reviews entre las fechas dadas
     #y, el porcentaje de recomendación de los mismos en base a reviews.recommend.
@@ -47,9 +47,9 @@ def countreviews(fecha_inicio:str, fecha_fin:str):
     total_r = df_reviews[(df_reviews['posted'] >= fecha_inicio) &
                      (df_reviews['posted'] <= fecha_fin)]['recommend'].count() # Y aca las recomendaciones sin importar cual son
     percentage = pos/total_r # Hacemos el porcentaje
-    return f'{len(users)}, {round(percentage,2)*100}%' # Y retornamos
+    return f'<p>{len(users)} : {round(percentage,2)*100}%</p>' # Y retornamos
 
-@app.get('/genre/{genero}')
+@app.get('/genre/{genero}', response_class=HTMLResponse)
 def genre(genero: str): # Esta funcion es a que mas se demora
     #def genre( género : str ): Devuelve el puesto en el que se encuentra un género
     #sobre el ranking de los mismos analizado bajo la columna PlayTimeForever.
@@ -61,9 +61,9 @@ def genre(genero: str): # Esta funcion es a que mas se demora
         sumas_por_genero[gen] = suma #Obtenemos la suma de ese genero y la agregamos al diccionario
     sumas_por_genero_ordenado = dict(sorted(sumas_por_genero.items(), key=lambda item: item[1], reverse=True)) # Creamos el ranking
     ranking = list(sumas_por_genero_ordenado.keys()).index(genero) + 1 # Conseguimos el puesto de nuestro genero
-    return f'{genero} : {ranking}' # Retornamos
+    return f'<p>{genero} : {ranking}</p>' # Retornamos
 
-@app.get('/userforgenre/{genero}')
+@app.get('/userforgenre/{genero}', response_class=HTMLResponse)
 def userforgenre(genero: str):
     df_games = pd.read_parquet('clean_games_functions.parquet.gzip')
     #def userforgenre( género : str ): Top 5 de usuarios con más horas de juego en el género dado,
@@ -75,7 +75,7 @@ def userforgenre(genero: str):
     top = '' # Este va a ser nuestro resultado
     for user_id, total_hours in top_users.items(): # Recorremos el top 5
         user_info = df_items[df_items['user_id'] == user_id].iloc[0] # Extraemos la informacion necesaria del usuario
-        top+=f'{user_info.user_id}, {total_hours}, {user_info.user_url}' # Y agregamos al resultado, el user, las horas y el url
+        top+=f'<p>{user_info.user_id}, {total_hours/60}, {user_info.user_url}</p>' # Y agregamos al resultado, el user, las horas y el url
     return top # Retornamos
 
 @app.get('/developer/{company_name}', response_class=HTMLResponse)
@@ -95,7 +95,7 @@ def developer(company_name: str):
         answer += f"<p>| {year} | {percentage*100:.0f}% |</p>" # Sumamos a nuestra respuesta, la informacion por año
     return answer # Retornamos
 
-@app.get('/sentiment_analysis/{year}')
+@app.get('/sentiment_analysis/{year}', response_class=HTMLResponse)
 def sentiment_analysis(year: str):
     #def sentiment_analysis( año : int ): Según el año de lanzamiento,
     #se devuelve una lista con la cantidad de registros de reseñas de usuarios
@@ -104,9 +104,9 @@ def sentiment_analysis(year: str):
     year_data = df_reviews[df_reviews['year'] == year] # Coseguimos los datos de nuestro año
     sentiment_mapping = {0: 'Negative', 1: 'Neutral', 2: 'Positive'} # Mapeamos los sentimientos con su respectiva palabra
     sentiment_counts = year_data['sentiment_analysis'].map(sentiment_mapping).value_counts().to_dict() # Contamos esos sentimientos
-    return sentiment_counts # Y los retornamos
+    return f'<p>{sentiment_counts}</p>' # Y los retornamos
 
-@app.get('/recomendacion_juego/{id_de_producto}')
+@app.get('/recomendacion_juego/{id_de_producto}', response_class=HTMLResponse)
 def recomendacion_juego(id_del_producto: str):
     #def recomendacion_juego( id de producto ): Ingresando el id de producto,
     # deberíamos recibir una lista con 5 juegos recomendados similares al ingresado.
@@ -117,4 +117,7 @@ def recomendacion_juego(id_del_producto: str):
     recommended_items = sorted(items_similares, key=lambda x: x[1], reverse=True) # Ahora ordenamos para saber nuestros items mas recomendados
     indices = [index for index, _ in recommended_items[1:10]] # Extraemos los indices de los juegos
     recommended_items = df_games.iloc[indices]['id'].tolist() # Convertimos a listas con nuestros ids, (podriamos poner nuestros app_name)
-    return recommended_items # Retornamos
+    recomedations = ""
+    for i in recommended_items[:5]:
+        recomedations+='<p>{i}</p>'
+    return recomedations # Retornamos los primeros 5
